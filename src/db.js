3760d18e5738a5cb1b3ce938730b5411618002ac
Mateus <mateusmenezesmartinelli@gmail.com>
2023-01-30 19:01:01 -0800

    fixed

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import { createPool } from "mysql2/promise";
 export const pool = createPool({
   host: "localhost",
   user: "root",
-  password: "faztpassword",
+  password: "m1a2t3e4u5s6",
   port: 3306,
   database: "customersdb",
 });
