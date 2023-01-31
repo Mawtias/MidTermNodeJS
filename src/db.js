bd650c53f46f051a28f9136eb76cb6867a05ab19
Mateus <mateusmenezesmartinelli@gmail.com>
2023-01-30 19:29:16 -0800

    remote sql created

--- src/db.js
+++ src/db.js
@@ -1,9 +1,13 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "m1a2t3e4u5s6",
+  //host: "localhost",
+  host: "sql9.freemysqlhosting.net",
+  //user: "root",
+  user: "sql9594540",
+  //password: "m1a2t3e4u5s6",
+  password: "WUjvb4ZfGx",
   port: 3306,
-  database: "customersdb",
+  //database: "customersdb",
+  database: "sql9594540"
 });
